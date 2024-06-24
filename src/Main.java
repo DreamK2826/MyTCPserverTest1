71fb1a95caaef3326d0ede13388828314971ca5c
DreamK2826 <jurui2826@qq.com>
2024-06-24 21:34:17 +0800

    服务端V0.1.03 20240624暂存版本1

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,49 @@ import java.util.List;
 
 
 public class Main {
-    private static List<String> strList2;
-    private static boolean flagOK = false;
-    private static List<APdata> aPdata;
-    private static List<TableData> tableDataList;
+    private static volatile List<String> strList2;
+    private static volatile boolean flagOK = false;
+    private static volatile List<APdata> aPdata;
+    private static volatile List<TableData> tableDataList;
 
     public static void main(String[] args) throws IOException {
 
         TCP_server(22441);
+        new Thread(() -> {
+            while(true){
+                if(flagOK){
+                    System.out.println("FLAG_OK");
+                    //切割处理部分
+                    if(strList2 != null){
+                        for (int i = 0; i < strList2.size(); i++) {
+                            if(i == 1){
+                                System.out.println(strList2.get(i).substring(5));
+                            }
+                            if(i == 2){
+                                System.out.println(strList2.get(i).substring(5));
+                            }
+                            if(i == 3){
+                                System.out.println(strList2.get(i).substring(5));
+
+                            }
+                            //从3号元素开始是wifi信息
+                            if (i > 3 && i < strList2.size() - 1) {
+                                String str = strList2.get(i), temp1,temp2;
+                                List<String> strL = Arrays.stream(str.split(",")).toList();
+                                temp1 = strL.getFirst();
+                                temp2 = strL.get(1);
+                                temp1 = temp1.substring(1);
+                                temp2 = temp2.substring(0,temp2.length()-1);
+//                                    System.out.println("#" + (i-3) + ":" + temp1 + "~" + temp2);
+                                aPdata.add(new APdata(temp1,Integer.parseInt(temp2)));
+                            }
+                        }
+                    }
+                    flagOK = false;
+                }
+            }
+
+        }).start();
 
     }
 
@@ -32,7 +67,6 @@ public class Main {
                     //侦听并接受到此套接字的连接，返回一个socket对象
                     Socket s = ss.accept();
                     //获取到输入流
-
                     InputStream is = s.getInputStream();
 //                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
                     //接收收到的数据
@@ -48,34 +82,10 @@ public class Main {
                         //判断到达末尾，切割为List，清空s1
                         if(temp.endsWith("$")){
                             strList2 = Arrays.stream(s1.toString().split("\0")).toList();
-                            System.out.println(strList2);
+//                            System.out.println(strList2);
                             s1.delete(0,s1.length());
+                            flagOK = true;
                         }
-                        if(strList2 != null){
-                            for (int i = 0; i < strList2.size(); i++) {
-                                if(i == 1){
-                                    System.out.println(strList2.get(i).substring(5));
-                                }
-                                if(i == 2){
-                                    System.out.println(strList2.get(i).substring(5));
-                                }
-                                if(i == 3){
-                                    System.out.println(strList2.get(i).substring(5));
-
-                                }
-                                if (i > 3 && i < strList2.size() - 1) {
-                                    String str = strList2.get(i), temp1,temp2;
-                                    List<String> strL = Arrays.stream(str.split(",")).toList();
-                                    temp1 = strL.getFirst();
-                                    temp2 = strL.get(1);
-                                    temp1 = temp1.substring(1);
-                                    temp2 = temp2.substring(0,temp2.length()-1);
-                                    System.out.println("#" + (i-3) + ":" + temp1 + "~" + temp2);
-                                    aPdata.add(new APdata(temp1,Integer.parseInt(temp2)));
-                                }
-                            }
-                        }
-
                     }
 
 
--- src/TableData.java
+++ src/TableData.java
@@ -1,20 +1,53 @@
-import java.util.List;
+import java.util.Date;
 
 /**
  * 用于生成表格的数据
  */
+
 public class TableData {
 
-    String name;
-    String pos1;
-    String pos2;
-    List<String> apDATA;
 
-    public TableData(String name, String pos1, String pos2, List<String> apDATA) {
+    //用于储存采集点信息
+    private String name;
+    private String pos1;
+    private String pos2;
+    private Date dateNow;
+    //用于储存AP强度信息
+    private String AP01;
+    private String AP02;
+    private String AP03;
+    private String AP04;
+    private String AP05;
+    private String AP06;
+    private String AP07;
+    private String AP08;
+    private String AP09;
+    private String AP10;
+    private String AP11;
+    private String AP12;
+    private String AP13;
+    private String AP14;
+
+    public TableData() {}
+    public TableData(String name, String pos1, String pos2, Date date, String AP01, String AP02, String AP03, String AP04, String AP05, String AP06, String AP07, String AP08, String AP09, String AP10, String AP11, String AP12, String AP13, String AP14) {
         this.name = name;
         this.pos1 = pos1;
         this.pos2 = pos2;
-        this.apDATA = apDATA;
+        dateNow = date;
+        this.AP01 = AP01;
+        this.AP02 = AP02;
+        this.AP03 = AP03;
+        this.AP04 = AP04;
+        this.AP05 = AP05;
+        this.AP06 = AP06;
+        this.AP07 = AP07;
+        this.AP08 = AP08;
+        this.AP09 = AP09;
+        this.AP10 = AP10;
+        this.AP11 = AP11;
+        this.AP12 = AP12;
+        this.AP13 = AP13;
+        this.AP14 = AP14;
     }
 
     public String getName() {
@@ -41,11 +74,123 @@ public class TableData {
         this.pos2 = pos2;
     }
 
-    public List<String> getApDATA() {
-        return apDATA;
+    public Date getDateNow() {
+        return dateNow;
     }
 
-    public void setApDATA(List<String> apDATA) {
-        this.apDATA = apDATA;
+    public void setDateNow(Date dateNow) {
+        this.dateNow = dateNow;
+    }
+
+    public String getAP01() {
+        return AP01;
+    }
+
+    public void setAP01(String AP01) {
+        this.AP01 = AP01;
+    }
+
+    public String getAP02() {
+        return AP02;
+    }
+
+    public void setAP02(String AP02) {
+        this.AP02 = AP02;
+    }
+
+    public String getAP03() {
+        return AP03;
+    }
+
+    public void setAP03(String AP03) {
+        this.AP03 = AP03;
+    }
+
+    public String getAP04() {
+        return AP04;
+    }
+
+    public void setAP04(String AP04) {
+        this.AP04 = AP04;
+    }
+
+    public String getAP05() {
+        return AP05;
+    }
+
+    public void setAP05(String AP05) {
+        this.AP05 = AP05;
+    }
+
+    public String getAP06() {
+        return AP06;
+    }
+
+    public void setAP06(String AP06) {
+        this.AP06 = AP06;
+    }
+
+    public String getAP07() {
+        return AP07;
+    }
+
+    public void setAP07(String AP07) {
+        this.AP07 = AP07;
+    }
+
+    public String getAP08() {
+        return AP08;
+    }
+
+    public void setAP08(String AP08) {
+        this.AP08 = AP08;
+    }
+
+    public String getAP09() {
+        return AP09;
+    }
+
+    public void setAP09(String AP09) {
+        this.AP09 = AP09;
+    }
+
+    public String getAP10() {
+        return AP10;
+    }
+
+    public void setAP10(String AP10) {
+        this.AP10 = AP10;
+    }
+
+    public String getAP11() {
+        return AP11;
+    }
+
+    public void setAP11(String AP11) {
+        this.AP11 = AP11;
+    }
+
+    public String getAP12() {
+        return AP12;
+    }
+
+    public void setAP12(String AP12) {
+        this.AP12 = AP12;
+    }
+
+    public String getAP13() {
+        return AP13;
+    }
+
+    public void setAP13(String AP13) {
+        this.AP13 = AP13;
+    }
+
+    public String getAP14() {
+        return AP14;
+    }
+
+    public void setAP14(String AP14) {
+        this.AP14 = AP14;
     }
 }
